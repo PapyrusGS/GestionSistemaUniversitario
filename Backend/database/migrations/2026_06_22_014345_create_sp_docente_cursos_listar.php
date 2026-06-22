d5bd2feb15efe855ebbb1a2760d88275e2165e78
PapyrusGS <willibarb2502@gmail.com>
2026-06-22 19:19:33 -0400

    ya pues actualicen sus bases de datos

--- Backend/database/migrations/2026_06_22_014345_create_sp_docente_cursos_listar.php
+++ Backend/database/migrations/2026_06_22_014345_create_sp_docente_cursos_listar.php
@@ -10,33 +10,34 @@ return new class extends Migration
      * Instala el Stored Procedure definitivo en la base de datos de todo el equipo.
      */
     public function up(): void
-    {
-        // Eliminamos el procedimiento si ya existía para evitar colisiones de nombres
-        DB::unprepared("DROP PROCEDURE IF EXISTS sp_docente_cursos_listar;");
-        // Creamos el Stored Procedure adaptado estrictamente a tu diseño físico de tablas
-        DB::unprepared("
-            CREATE PROCEDURE sp_docente_cursos_listar(IN p_idDocente BIGINT UNSIGNED)
-            BEGIN
-                SELECT 
-                    cm.idCursoMateria,
-                    cm.idCurso,
-                    cm.idMateria,
-                    cm.fechaInicio,
-                    cm.fechaFin,
-                    cm.maxInscritos,
-                    m.nombre AS materia_nombre,
-                    IFNULL(
-                        (SELECT GROUP_CONCAT(CONCAT(h.diaSemana, ' (', TIME_FORMAT(h.horaInicio, '%H:%i'), '-', TIME_FORMAT(h.horaFin, '%H:%i'), ')') SEPARATOR ' / ')
-                         FROM horariocurso hc
-                         INNER JOIN horarios h ON hc.idHorario = h.idHorario
-                         WHERE hc.idCurso = cm.idCurso), 
-                        'Sin horario'
-                    ) AS turno_nombre
-                FROM cursos_materias cm
-                INNER JOIN materias m ON cm.idMateria = m.idMateria
-                WHERE cm.idDocente = p_idDocente AND cm.estado = 1;
-            END;
-        ");
+{
+    // Eliminamos el procedimiento si ya existía para evitar colisiones de nombres
+    DB::unprepared("DROP PROCEDURE IF EXISTS sp_docente_cursos_listar;");
+
+    // Creamos el Stored Procedure adaptado a tu NUEVO diseño de tablas
+    DB::unprepared("
+        CREATE PROCEDURE sp_docente_cursos_listar(IN p_idDocente BIGINT UNSIGNED)
+        BEGIN
+            SELECT 
+                cm.idCursoMateria,
+                cm.idCurso,
+                cm.idMateria,
+                cm.fechaInicio,
+                cm.fechaFin,
+                m.nombre AS materia_nombre,
+                IFNULL(
+                    (SELECT GROUP_CONCAT(CONCAT(h.diaSemana, ' (', TIME_FORMAT(h.horaInicio, '%H:%i'), '-', TIME_FORMAT(h.horaFin, '%H:%i'), ')') SEPARATOR ' / ')
+                     FROM horariocurso hc
+                     INNER JOIN horarios h ON hc.idHorario = h.idHorario
+                     WHERE hc.idCursoMateria = cm.idCursoMateria), 
+                    'Sin horario'
+                ) AS turno_nombre
+            FROM cursos_materias cm
+            INNER JOIN materias m ON cm.idMateria = m.idMateria
+            WHERE cm.idDocente = p_idDocente AND cm.estado = 1;
+        END;
+    ");
+
     }
 
     /**
